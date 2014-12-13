c8a9627135689508a1ec53a039a23fde02ca7601
Le Anh Quan <quanla2003@gmail.com>
2014-12-13 11:58:47 +0700

    Add Maven depedencies

--- src/main/java/qj/blog/classreloading/example1/StaticInt.java
+++ src/main/java/qj/blog/classreloading/example1/StaticInt.java
@@ -10,24 +10,25 @@ import qj.util.lang.DynamicClassLoader;
  */
 public class StaticInt {
 	public static void main(String[] args) {
-		Class<?> userClass = new DynamicClassLoader("target/classes")
+		Class<?> userClass1 = User.class;
+		Class<?> userClass2 = new DynamicClassLoader("target/classes")
 				.load("qj.blog.classreloading.example1.StaticInt$User");
 
 
 		out.println("Seems to be the same class:");
-		out.println(userClass.getName());
-		out.println(User.class.getName());
+		out.println(userClass1.getName());
+		out.println(userClass2.getName());
 		out.println();
 
 		out.println("But why there are 2 different class loaders:");
-		out.println(userClass.getClassLoader());
-		out.println(User.class.getClassLoader());
+		out.println(userClass1.getClassLoader());
+		out.println(userClass2.getClassLoader());
 		out.println();
 
 		User.age = 11;
 		out.println("And different age values:");
-		out.println((int) ReflectUtil.getStaticFieldValue("age", User.class));
-		out.println((int) ReflectUtil.getStaticFieldValue("age", userClass));
+		out.println((int) ReflectUtil.getStaticFieldValue("age", userClass1));
+		out.println((int) ReflectUtil.getStaticFieldValue("age", userClass2));
 	}
 
 	public static class User {
---
+++ src/main/java/qj/blog/classreloading/example5/WebApp.java
@@ -0,0 +1,10 @@
+package qj.blog.classreloading.example5;
+
+/**
+ * Created by Quan on 13/12/2014.
+ */
+public class WebApp {
+	public static void main(String[] args) {
+		
+	}
+}
